6c540364972f15da76f82f3fb8e31e01a8f3c043
Eva Doncieux <eva.doncieux@gmail.com>
2023-11-06 11:54:22 +0100

    exo2 with Babel

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const helloWorld = React.createElement('h1', {}, 'Hello world!');
+const text = <h2>Coucou ?</h2>
 
 
 // mauvaise pratique
 
-
 //ReactDOM.render(helloWorld, document.body);
 
 ReactDOM.render(helloWorld, document.querySelector('#app'));
+ReactDOM.render(text, document.querySelector('#app'));
